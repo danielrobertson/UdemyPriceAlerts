1e17236b3d232ceb3811d36658857a85c9a9ad21
danielrobertson <danielrobertson733@gmail.com>
2016-06-07 22:34:40 -0500

    iterating course list

--- index.js
+++ index.js
@@ -11,56 +11,61 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-var clientId = process.env.UDEMY_CLIENT_ID; 
-var clientSecret = process.env.UDEMY_CLIENT_SECRET; 
+// get course information from Udemy 
+var clientId = process.env.UDEMY_CLIENT_ID;
+var clientSecret = process.env.UDEMY_CLIENT_SECRET;
+
 var options = {
-  url: "https://www.udemy.com/api-2.0/courses/",
-  headers: {
-    clientId: clientSecret
-  }
+    url: "https://www.udemy.com/api-2.0/courses/",
+    auth: {
+        user: clientId,
+        password: clientSecret
+    }
 };
 
-// get course information from Udemy 
-request(options, function (error, response, body) {
-	if (!error && response.statusCode == 200) {
+request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        connection.query("select * from price", function(err, rows, fields) {
+            if (err) {
+                throw err;
+            }
 
-		console.log("Data"); 
-		console.log(body); 
-		connection.query("select * from price", function(err, rows, fields) {
-		    if (err) {
-		        throw err;
-		    }
+            var courseList = JSON.parse(body).results;
+            courseList.forEach(function(course) {
+                var isNewCourse = true;
+                rows.forEach(function(row) {
 
-		    body.courses.forEach(function(course) {
-		        var isNewCourse = true;
-		        rows.forEach(function(row) {
+                    if (row.title === course.title) {
+                    	console.log("Not new course\n" + course.title); 
+                        console.log("Processing course: " + course.title + ", price: " + course.price);
+                        isNewCourse = false;
 
-		            if (row.title === course.title) {
-		                console.log("Processing course: " + course.title + ", price: " + course.price);
-		                isNewCourse = false;
+                        if (course.price < row.price) {
+                            console.log("\tPrice has dropped for course: " + course.title + " from " + row.price + " to " + course.price);
 
-		                if (course.price < row.price) {
-		                    console.log("\tPrice has dropped for course: " + course.title + " from " + row.price + " to " + course.price);
+                            // send alert to user about this price drop 
 
-		                    // send alert to user about this price drop 
+                            // update database with current price so that check can repeat tomorrow 
+                        }
+                    }
 
-		                    // update database with current price so that check can repeat tomorrow 
-		                }
-		            }
+                });
 
-		        });
+                if (isNewCourse) {
+                    // add course to datastore 
+                    console.log("New course found:");
+                    console.log(course); 
+                }
 
-		        if (isNewCourse) {
-		            // add course to datastore 
-		            console.log("New course found: " + course.title);
-		        }
-
-		    });
+            });
 
 
-		});
 
-		connection.end();
+        });
 
-	}
+        connection.end();
+
+    } else {
+        console.log("Error request course info from Udemy");
+    }
 });
